453c18b73a2c295b66b43000d9d93ba945dbcf36
Dave Jamir Basa <basajamir@gmail.com>
2024-09-29 15:49:19 +0800

    Add files via upload

---
+++ YaraDB/users/navigator.php
@@ -0,0 +1,37 @@
+<!-- NAVIGATOR BAR-->
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary py-3 fixed-top">
+
+    <div class="container">
+        <img class="logo" src="assets/imgs/logo.png"/>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button> 
+    <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="browse.php">Browse</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+        </li>
+        
+        <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+        </li>
+        
+        <li class="nav-item">
+            <a class="nav-link" href="#">Signup</a>
+        </li>
+
+        </ul>
+    </div>
+    </div>
+</nav>
+
